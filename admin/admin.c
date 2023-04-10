004fb1ac3d7ad46188ea64ff3c32858a07dc1240
mariam ehab <mariamehab26122003@gmail.com>
2023-04-10 14:22:01 +0200

    admin functions

--- admin/admin.c
+++ admin/admin.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+<<<<<<< Updated upstream
 #include "admin.h"
 int main()
 {
     //Student* arr = (Student*) malloc(classSize * sizeof( Student));
     struct Student **arr = malloc(classSize * sizeof(struct Student*));
     static int num_stu;
+=======
+#include<string.h>
+ #include "admin.h"
+    //Student* arr = (Student*) malloc(classSize * sizeof( Student));
+     Student **arr = malloc(classSize * sizeof( Student));
+    static int num_stu;
+    adminn *ad;
+
+>>>>>>> Stashed changes
     //implementation of add function.
     void Add_student (Student *st)
     {
@@ -85,6 +95,82 @@ int main()
         if(flag==0)
             printf("Invalid ID,please try again");
     }
+<<<<<<< Updated upstream
     return 0;
 }
 
+=======
+//view all records
+void viewAllrecords(){
+
+
+for (int i = 0; i < classSize; i++)
+    {
+        if (arr[i] != 0 || arr[i] != NULL)//check if it's valid Id so we can show data.
+
+        {
+            viewStu(arr[i]->id);
+        }
+
+}
+//edit admin password
+void EditAdminPass(){
+char *passwordd = (char *)malloc(strlen(ad->pass) + 1);
+    char *newpass = (char *)malloc(strlen(ad->pass) + 1);
+    printf("Please enter admin password\n");
+    scanf("%s",passwordd);
+
+
+
+ if(strcmp(passwordd,ad->pass)==0){//check if the password correct or not
+        ad->pass=NULL;
+         printf("Please enter new password :\n");
+        scanf("%s", newpass);
+        printf("admin Password %s\n", newpass);
+        ad->pass= newpass;
+        printf("admin Password %s\n", ad->pass);
+    }
+    else
+    {
+        printf("INCORRECT PASSWORD TRY AGAIN\n");
+    }
+    }
+}
+//edit student grade
+  void EditStuGrade(){
+      char *passs_=(char *)malloc(10*sizeof(char));
+      printf("Enter admin password\n");
+      scanf("%s",passs_);
+        if (strcmp(passs_, ad->pass) == 0){//check if the password correct or not
+        int id ,grade;
+        printf("Enter student id:\n");
+        scanf("%d",&id);
+         for (int i = 0; i < classSize; i++)
+        {
+            if (arr[i] != 0 || arr[i] != NULL)
+            {
+                if (arr[i]->id == id)
+                {
+            printf("student grade=%d\n",arr[i]->grade);
+            printf("edit the grade");
+            scanf("%d",&grade);
+            arr[i]->grade=grade;//update the grade of student
+
+        }
+            }
+            else{
+                 printf("student is not found\n");
+            }
+
+        }
+    }
+
+
+else{
+     printf("Enter correct password\n");
+}
+
+  }
+
+
+>>>>>>> Stashed changes
--- admin/admin.h
+++ admin/admin.h
@@ -1,3 +1,8 @@
+<<<<<<< Updated upstream
+=======
+
+#ifndef admin.h
+>>>>>>> Stashed changes
 #define admin.h
 #define  classSize 25
 typedef struct student  //fields of student structure.
@@ -9,10 +14,24 @@ typedef struct student  //fields of student structure.
     int age;
     char *password;
 } Student;
+<<<<<<< Updated upstream
 
 
 void Add_student (Student *st); //prototype of add function
 void remove_student (int id); //prototype of remove function
 void viewStu (int id);    //prototype of view student record function
+=======
+typedef struct Admin  //fields of student structure.
+{
+   char *pass;
+}adminn;
+
+void Add_student (Student *st); //prototype of add function
+void remove_student (int id); //prototype of remove function
+void viewStu (int id);
+void viewAllrecords();
+void EditAdminPass();
+void EditStuGrade();  //prototype of view student record function
+>>>>>>> Stashed changes
 
 #endif
